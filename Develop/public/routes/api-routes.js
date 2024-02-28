0652b577c8a6b70ebd57f3e7cf8b30e95ee5c715
Daniel John <dcampo85@gmail.com>
2024-02-28 09:32:24 -0500

    add server.js

--- Develop/public/routes/api-routes.js
+++ Develop/public/routes/api-routes.js
@@ -5,6 +5,8 @@ const fs = require("fs");
 // routing syntax
 module.exports = (app) => {
   app.get("/Develop/public/notes.html", (req, res) => {
-    res.sendFile(path.join(__dirname, "/Develop/db/db.json"));
+    res.sendFile(path.join(__dirname, "/Develop/db/db.json")); // send body parameter to the client
   });
+
+  app.post;
 };
---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const app = express();
+const uuid = require("/Develop/public/helpers/uuid");
+const PORT = 3001;
