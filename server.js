59272c3f2eb82f967e2e63ef3b3caeae82617d06
Daniel John <dcampo85@gmail.com>
2024-02-28 13:45:43 -0500

    server js update

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const uuid = require("/Develop/public/helpers/uuid");
 const PORT = 3001;
+
+// use express to create route files, encode url
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("/Develop/public/routes"));
+
+// route files
+require("/Develop/public/routes/api-routes")(app);
+require("/Develop/public/routes/html-routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`Server available at localhost${PORT}`);
+});
