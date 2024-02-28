66822ea0c9cb5b27398b19405cc85b1fc33adc8d
Daniel John <dcampo85@gmail.com>
2024-02-28 08:51:58 -0500

    routing syntax 1

--- Develop/public/routes/api-routes.js
+++ Develop/public/routes/api-routes.js
@@ -1,1 +1,10 @@
 // call dependencies
+const path = require("path");
+const fs = require("fs");
+
+// routing syntax
+module.exports = (app) => {
+  app.get("/Develop/public/notes.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Develop/db/db.json"));
+  });
+};
