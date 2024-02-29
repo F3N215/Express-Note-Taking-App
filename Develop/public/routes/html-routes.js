669c657753ac39ce095a502aeb0a5b878f4a75ce
Daniel John <dcampo85@gmail.com>
2024-02-29 17:53:29 -0500

    html-routes 2

--- Develop/public/routes/html-routes.js
+++ Develop/public/routes/html-routes.js
@@ -3,4 +3,9 @@ const path = require("path");
 
 module.exports = (app) => {
   app.get("/Develop/public/notes.html");
+  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
 };
+
+app.get("*", (req, res) => {
+  res.SendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
