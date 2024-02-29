14d6bc676c82390457bca8d78648fedafe14c316
Daniel John <dcampo85@gmail.com>
2024-02-29 16:46:48 -0500

    userNote

--- Develop/public/routes/api-routes.js
+++ Develop/public/routes/api-routes.js
@@ -8,9 +8,13 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, "/Develop/db/db.json")); // send body parameter to the client
   });
 
+  // uses POST to send new note to request and add to db.json, then return new note
   app.post("/Develop/public/notes.html", (req, res) => {
     let db = fs.readFileSync("/Develop/db/db.json");
     db = JSON.parse(db);
     res.json(db);
+    let userNote = {
+      title: req.body.title,
+    };
   });
 };
