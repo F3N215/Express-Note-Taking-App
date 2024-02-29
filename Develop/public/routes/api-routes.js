6eb4287716ff86ccaf6e29d53729447dc8224a5d
Daniel John <dcampo85@gmail.com>
2024-02-29 11:42:54 -0500

    app.post

--- Develop/public/routes/api-routes.js
+++ Develop/public/routes/api-routes.js
@@ -8,5 +8,9 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, "/Develop/db/db.json")); // send body parameter to the client
   });
 
-  app.post;
+  app.post("/Develop/public/notes.html", (req, res) => {
+    let db = fs.readFileSync("/Develop/db/db.json");
+    db = JSON.parse(db);
+    res.json(db);
+  });
 };
