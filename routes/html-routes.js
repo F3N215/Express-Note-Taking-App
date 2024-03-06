4c2c59bc46111a2bbfdfedd63a1d3341ad883c41
Daniel John <dcampo85@gmail.com>
2024-03-06 17:11:44 -0500

    path corrections

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,11 +3,11 @@ const path = require("path");
 
 // GET returns notes.html file
 module.exports = (app) => {
-  app.get("/Develop/public/notes.html");
-  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+  app.get("/public/notes.html");
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 };
 
 // GET * to return index.html file
 app.get("*", (req, res) => {
-  res.SendFile(path.join(__dirname, "/Develop/public/index.html"));
+  res.SendFile(path.join(__dirname, "/"));
 });
