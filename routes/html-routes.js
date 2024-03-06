f9bb6f45a0ecbcb01d13edf0a3e8989620ec0822
Daniel John <dcampo85@gmail.com>
2024-03-06 17:32:09 -0500

    adjust path and function syntax

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,11 +3,12 @@ const path = require("path");
 
 // GET returns notes.html file
 module.exports = (app) => {
-  app.get("/public/notes.html");
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-};
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
 
-// GET * to return index.html file
-app.get("*", (req, res) => {
-  res.SendFile(path.join(__dirname, "/"));
-});
+  // GET * to return index.html file
+  app.get("*", (req, res) => {
+    res.SendFile(path.join(__dirname, "/"));
+  });
+};
